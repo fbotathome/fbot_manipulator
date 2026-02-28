2b558ad12fd3cae24628671500dcb0eda15871fd
Luizlost01 <luized_sparvoli@hotmail.com>
2026-02-28 17:04:44 -0300

    🚧OH SHIT

--- fbot_manipulator/src/utils/save_arm_pose.cpp
+++ fbot_manipulator/src/utils/save_arm_pose.cpp
@@ -1,32 +1,46 @@
 #include <memory>
 
-#include <rclcpp/rclcpp.hpp>
+#include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
-{
+// class GetArmPose : public rclcpp::Node
+// {
+//   public:
+//     GetArmPose()
+//     : Node("getarmpose")
+//     {
+//       subscription_ = this->create_subscription<std_msgs::msg::String>(
+//       "xarm", 10, std::bind(&GetArmPose::topic_callback, this, _1));
+//     }
+
+//   private:
+//     void topic_callback(const std_msgs::msg::String & msg) const
+//     {
+//       RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+//     }
+//     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+// };
+
+class GetArmPose : public rclcpp::Node {
   public:
-    MinimalSubscriber()
-    : Node("minimal_subscriber")
-    {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    GetArmPose() : Node("getarmpose") {
+      subscription_ = this->create_subscription<std_msgs::msg::Strings>("joint_states" 10, std::bin(&GetArmPose::jointStates_callback, this, _1));
     }
 
-  private:
-    void topic_callback(const std_msgs::msg::String & msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
-    }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    private: 
+      void jointStates_callback(const std_msgs::msg::String & msg) const {
+        RCLCPP_INFO(this->get_logger(), "I Heard: %s", msg.data.c_str());
+      }
+      rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<GetArmPose>());
   rclcpp::shutdown();
   return 0;
 }
+
