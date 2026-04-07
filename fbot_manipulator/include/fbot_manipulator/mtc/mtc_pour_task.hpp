685a57301a5b5456973c83e71f13a10e87409258
luizlost01 <luized_sparvoli@hotmail.com>
2026-04-07 19:11:52 -0300

    Changed due Copilot PR Request

--- fbot_manipulator/include/fbot_manipulator/mtc/mtc_pour_task.hpp
+++ fbot_manipulator/include/fbot_manipulator/mtc/mtc_pour_task.hpp
@@ -8,20 +8,20 @@ namespace fbot_manipulator
 class MtcPourTask : public MtcTask
 {
 public:
-	MtcPourTask(rclcpp::Node::SharedPtr node,
-				const std::string& object_id,
-				const geometry_msgs::msg::Pose& place_pose);
+  MtcPourTask(rclcpp::Node::SharedPtr node,
+              const std::string& object_id,
+              const geometry_msgs::msg::Pose& place_pose);
 
-	MtcPourTask(rclcpp::Node::SharedPtr node,
-				const std::string& object_id,
-				const std::string& place_pose_name);
+  MtcPourTask(rclcpp::Node::SharedPtr node,
+              const std::string& object_id,
+              const std::string& place_pose_name);
 
-	bool buildTask() override;
+  bool buildTask() override;
 
 private:
-	std::string object_id_;
-	geometry_msgs::msg::Pose place_pose_;
-	std::string place_pose_name_;
+  std::string object_id_;
+  geometry_msgs::msg::Pose place_pose_;
+  std::string place_pose_name_;
 };
 
 } // namespace fbot_manipulator
